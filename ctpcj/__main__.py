ab7c1f5110d4d9fc30b9b7d40c50b12d91308aa9
Swen Kooij <sk@bigbrother.nl>
2015-11-05 15:27:27 +0200

    Small fix in __main__.py for relative import

--- ctpcj/__main__.py
+++ ctpcj/__main__.py
@@ -1,7 +1,7 @@
 """Simple entry point of the application, does nothing more
 than construct an Application instance and run it."""
 
-from ctpcj import Application
+from .application import Application
 
 def main():
     """Called when the application is starting, runs the web server."""
